a1aef6cb08159784907b9b395e463a13e6c4804c
Ryan Roberts <71022571+ryand500@users.noreply.github.com>
2024-07-24 13:32:09 +0200

    Update index.tsx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+//import reportWebVitals from './reportWebVitals'
 import './i18n'
 import './static/index.css'
 import './static/globals.css'
 
-import './setup/sentry_init'
+//import './setup/sentry_init'
 
-import './setup/ga_init'
+//import './setup/ga_init'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -20,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+//reportWebVitals()
