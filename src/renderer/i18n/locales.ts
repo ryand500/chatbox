36a234524c5cdc35ee9dac6c08cc01f829229f02
Ryan Roberts <71022571+ryand500@users.noreply.github.com>
2024-07-24 13:50:02 +0200

    Update locales.ts

--- src/renderer/i18n/locales.ts
+++ src/renderer/i18n/locales.ts
@@ -2,13 +2,13 @@ import { Language } from '../../shared/types'
 
 export const languageNameMap = {
     en: 'English',
-    'zh-Hans': '简体中文',
-    'zh-Hant': '繁體中文',
-    ja: '日本語',
-    ko: '한국어',
-    ru: 'Русский', // Russian
+    //'zh-Hans': '简体中文',
+    //'zh-Hant': '繁體中文',
+    //ja: '日本語',
+    //ko: '한국어',
+    //ru: 'Русский', // Russian
     de: 'Deutsch', // German
-    fr: 'Français', // French
+    //fr: 'Français', // French
 }
 
 export const languages = Array.from(Object.keys(languageNameMap)) as Language[]
